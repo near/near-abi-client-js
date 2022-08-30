fdca292d978e71136c1459e81c4cce74f78f73ff
Daniyar Itegulov <ditegulov@gmail.com>
2022-08-30 19:45:19 +1000

    chore: update abi models to follow the new schema format (#4)

--- src/abi.ts
+++ src/abi.ts
@@ -1,40 +1,82 @@
+// TODO: Replace this file with near-abi-js when it is released
 import { JSONSchema7 } from 'json-schema';
 
-export interface ABI {
-    abi_schema_version: string;
-    metadata?: ContractMetadata;
-    abi: ABIData;
+/** Root model representing the entire contract ABI with all its functions and custom types. */
+export interface AbiRoot {
+    /** Semver of the ABI schema format. */
+    schema_version: string;
+    /** Metadata information about the contract. */
+    metadata?: AbiMetadata;
+    /** Core ABI information (functions and types). */
+    body: AbiBody;
 }
 
-export interface ABIData {
-    functions: ABIFunction[];
-    /** Root JSON schema for the ABI */
-    root_schema: JSONSchema7;
-}
-
-export interface ContractMetadata {
+/** Metadata information about the contract. */
+export interface AbiMetadata {
+    /** The name of the smart contract. */
     name?: string;
+    /** The version of the smart contract. */
     version?: string;
+    /** The authors of the smart contract. */
     authors?: string[];
 }
 
-export interface ABIFunction {
+/** Core ABI information. */
+export interface AbiBody {
+    /** ABIs of all contract's functions. */
+    functions: AbiFunction[];
+    /** Root JSON schema for the ABI. */
+    root_schema: JSONSchema7;
+}
+
+/** ABI of a single function. */
+export interface AbiFunction {
     name: string;
+    /** Human-readable documentation parsed from the source file. */
+    doc?: string;
+    /** Whether function does not modify the state. */
     is_view?: boolean;
+    /** Whether function can be used to initialize the state. */
     is_init?: boolean;
+    /** Whether function is accepting $NEAR. */
     is_payable?: boolean;
+    /** Whether function can only accept calls from self (current account). */
     is_private?: boolean;
-    params?: ABIParameterInfo[];
+    /** Type identifiers of the function parameters. */
+    params?: AbiParameter[];
+    /** Type identifiers of the callbacks of the function. */
     callbacks?: any[];
-    callbacks_vec?: ABITypeInfo;
-    result?: ABITypeInfo;
+    /** Type identifier of the vararg callbacks of the function. */
+    callbacks_vec?: AbiType;
+    /** Return type identifier. */
+    result?: AbiType;
 }
 
-export interface ABITypeInfo {
-    type_schema: JSONSchema7;
+/** Information about a single type (e.g. function return type). */
+export interface AbiType {
+    /** The serialization format to be used for values of this type. */
     serialization_type: string;
+    /** Schema describing the type (the actual schema format depends on `serialization_type`). */
+    type_schema: any;
 }
 
-export interface ABIParameterInfo extends ABITypeInfo {
+/** ABI type which values are serialized using JSON format. */
+export interface AbiJsonType extends AbiType {
+    serialization_type: "json";
+    /** JSON Subschema that represents this type (can be an inline primitive, a reference to the root schema and a few other corner-case things). */
+    type_schema: JSONSchema7;
+}
+
+/** ABI type which values are serialized using Borsh format. */
+export interface AbiBorshType extends AbiType {
+    serialization_type: "borsh";
+    // TODO: Add a TS model for Borsh schema to borsh-js
+    /** Borsh schema that represents this type. */
+    type_schema: any;
+}
+
+/** Information about a single named function parameter. */
+export interface AbiParameter extends AbiType {
+    /** Parameter name (e.g. `p1` in `function foo(p1: string) {}`). */
     name: string;
 }
--- test/testSchema.ts
+++ test/testSchema.ts
@@ -1,140 +1,189 @@
-import { ABI } from "../src";
+import { AbiRoot } from "../src";
 
 // TODO for some reason the TS json module resolver fails to match exact patterns for types
 // TODO which in this case fails for "type" field.
-export const testSchema: ABI = {
-	"abi_schema_version": "0.1.0",
-	"metadata": {
-	  "name": "abi",
-	  "version": "0.1.0",
-	  "authors": [
-		"Near Inc <hello@nearprotocol.com>"
-	  ]
-	},
-	"abi": {
-	  "functions": [
-		{
-		  "name": "add",
-		  "is_view": true,
-		  "params": [
-			{
-			  "name": "a",
-			  "type_schema": {
-				"$ref": "#/definitions/Pair"
-			  },
-			  "serialization_type": "json"
-			},
-			{
-			  "name": "b",
-			  "type_schema": {
-				"$ref": "#/definitions/Pair"
-			  },
-			  "serialization_type": "json"
-			},
-			{
-			  "name": "c",
-			  "type_schema": {
-				"type": "integer",
-				"format": "uint8",
-				"minimum": 0.0
-			  },
-			  "serialization_type": "json"
-			}
-		  ],
-		  "result": {
-			"type_schema": {
-			  "$ref": "#/definitions/Pair"
-			},
-			"serialization_type": "json"
-		  }
-		},
-		{
-		  "name": "add_callback",
-		  "is_view": true,
-		  "callbacks": [
-			{
-			  "type_schema": {
-				"$ref": "#/definitions/DoublePair"
-			  },
-			  "serialization_type": "json"
-			},
-			{
-			  "type_schema": {
-				"$ref": "#/definitions/DoublePair"
-			  },
-			  "serialization_type": "json"
-			}
-		  ],
-		  "callbacks_vec": {
-			"type_schema": {
-			  "type": "array",
-			  "items": {
-				"$ref": "#/definitions/DoublePair"
-			  }
-			},
-			"serialization_type": "json"
-		  },
-		  "result": {
-			"type_schema": {
-			  "$ref": "#/definitions/DoublePair"
-			},
-			"serialization_type": "json"
-		  }
-		},
-		{
-			"name": "no_params",
-			"is_view": true,
-			"result": {
-			  "type_schema": {
-				"type": "object",
-				"additionalProperties": {
-				  "type": "integer",
-				  "format": "uint64",
-				  "minimum": 0.0
-				}
-			  },
-			  "serialization_type": "json"
-			}
-		  }
-	  ],
-	  "root_schema": {
-		"$schema": "http://json-schema.org/draft-07/schema#",
-		"title": "String",
-		"type": "string",
-		"definitions": {
-		  "DoublePair": {
-			"type": "object",
-			"required": [
-			  "first",
-			  "second"
-			],
-			"properties": {
-			  "first": {
-				"$ref": "#/definitions/Pair"
-			  },
-			  "second": {
-				"$ref": "#/definitions/Pair"
-			  }
-			}
-		  },
-		  "Pair": {
-			"type": "array",
-			"items": [
-			  {
-				"type": "integer",
-				"format": "uint32",
-				"minimum": 0.0
-			  },
-			  {
-				"type": "integer",
-				"format": "uint32",
-				"minimum": 0.0
-			  }
-			],
-			"maxItems": 2,
-			"minItems": 2
-		  }
-		}
-	  }
-	}
-  };
+export const testSchema: AbiRoot = {
+  "schema_version": "0.1.0",
+  "metadata": {
+    "name": "abi",
+    "version": "0.1.0",
+    "authors": [
+      "Near Inc <hello@nearprotocol.com>"
+    ]
+  },
+  "body": {
+    "functions": [
+      {
+        "name": "add",
+        "doc": " Adds two pairs point-wise.",
+        "is_view": true,
+        "params": [
+
+          {
+            "name": "a",
+            "serialization_type": "json",
+            "type_schema": {
+              "$ref": "#/definitions/Pair"
+            }
+          },
+          {
+            "name": "b",
+            "serialization_type": "json",
+            "type_schema": {
+              "$ref": "#/definitions/Pair"
+            }
+          },
+          {
+            "name": "c",
+            "serialization_type": "json",
+            "type_schema": {
+              "type": "integer",
+              "format": "uint8",
+              "minimum": 0.0
+            }
+          }
+        ],
+        "result": {
+          "serialization_type": "json",
+          "type_schema": {
+            "$ref": "#/definitions/Pair"
+          }
+        }
+      },
+      {
+        "name": "add_borsh",
+        "is_view": true,
+        "params": [
+          {
+            "name": "a",
+            "serialization_type": "borsh",
+            "type_schema": {
+              "declaration": "Pair",
+              "definitions": {
+                "Pair": {
+                  "Struct": [
+                    "u32",
+                    "u32"
+                  ]
+                }
+              }
+            }
+          },
+          {
+            "name": "b",
+            "serialization_type": "borsh",
+            "type_schema": {
+              "declaration": "Pair",
+              "definitions": {
+                "Pair": {
+                  "Struct": [
+                    "u32",
+                    "u32"
+                  ]
+                }
+              }
+            }
+          }
+        ],
+        "result": {
+          "serialization_type": "borsh",
+          "type_schema": {
+            "declaration": "Pair",
+            "definitions": {
+              "Pair": {
+                "Struct": [
+                  "u32",
+                  "u32"
+                ]
+              }
+            }
+          }
+        }
+      },
+      {
+        "name": "add_callback",
+        "is_view": true,
+        "callbacks": [
+          {
+            "serialization_type": "json",
+            "type_schema": {
+              "$ref": "#/definitions/DoublePair"
+            }
+          },
+          {
+            "serialization_type": "json",
+            "type_schema": {
+              "$ref": "#/definitions/DoublePair"
+            }
+          }
+        ],
+        "callbacks_vec": {
+          "serialization_type": "json",
+          "type_schema": {
+            "$ref": "#/definitions/DoublePair"
+          }
+        },
+        "result": {
+          "serialization_type": "json",
+          "type_schema": {
+            "$ref": "#/definitions/DoublePair"
+          }
+        }
+      },
+      {
+        "name": "no_params",
+        "is_view": true,
+        "result": {
+          "serialization_type": "json",
+          "type_schema": {
+            "type": "object",
+            "additionalProperties": {
+              "type": "integer",
+              "format": "uint64",
+              "minimum": 0.0
+            }
+          }
+        }
+      }
+    ],
+    "root_schema": {
+      "$schema": "http://json-schema.org/draft-07/schema#",
+      "title": "String",
+      "type": "string",
+      "definitions": {
+        "DoublePair": {
+          "type": "object",
+          "required": [
+            "first",
+            "second"
+          ],
+          "properties": {
+            "first": {
+              "$ref": "#/definitions/Pair"
+            },
+            "second": {
+              "$ref": "#/definitions/Pair"
+            }
+          }
+        },
+        "Pair": {
+          "type": "array",
+          "items": [
+            {
+              "type": "integer",
+              "format": "uint32",
+              "minimum": 0.0
+            },
+            {
+              "type": "integer",
+              "format": "uint32",
+              "minimum": 0.0
+            }
+          ],
+          "maxItems": 2,
+          "minItems": 2
+        }
+      }
+    }
+  }
+};
